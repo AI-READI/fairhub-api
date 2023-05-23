2d6364c751c2e70b3f103c173d6bff9269485274
aydawka <aydan.gasimova2@gmail.com>
2023-05-23 00:07:26 -0700

    feat: added POST endpoints

---
+++ apis/updateStudies.py
@@ -0,0 +1,42 @@
+from __main__ import app
+from flask import request
+from flask import jsonify
+
+@app.route('/study', methods=['POST'])
+def updateStudies():
+    data = request.json
+    data["id"] = 3
+    return jsonify(data), 201
+
+
+@app.route('/study/<studyId>', methods=['POST'])
+def updateStudy(studyId):
+    data = request.json
+    return jsonify(data), 200
+
+
+@app.route('/study/<studyId>/dataset', methods=['POST'])
+def updateDatesets():
+    data = request.json
+    data["id"] = 3
+    return jsonify(data), 201
+
+
+@app.route('/study/<studyId>/dataset/<datasetId>', methods=['POST'])
+def updateDataset(studyId, datasetId):
+    data = request.json
+    return jsonify(data), 200
+
+
+@app.route('/study/<studyId>/dataset/<datasetId>/version', methods=['POST'])
+def updateDatesetVersions(studyId, datasetId):
+    data = request.json
+    data["id"] = 3
+    return jsonify(data), 201
+
+
+@app.route('/study/<studyId>/dataset/<datasetId>/version/<versionId>', methods=['POST'])
+def updateDatesetVersion(studyId, datasetId, versionId):
+    data = request.json
+    data["id"] = 3
+    return jsonify(data), 201
