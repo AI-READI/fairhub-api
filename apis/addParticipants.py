db30467d25edbc4a237e3d77ecac7a6813a5bb88
aydawka <aydan.gasimova2@gmail.com>
2023-05-23 00:07:06 -0700

    feat: added GET endpoints

---
+++ apis/addParticipants.py
@@ -0,0 +1,19 @@
+from __main__ import app
+
+
+from faker import Faker
+
+@app.route('/participants', methods=['GET'])
+def get_participants():
+    fake = Faker()
+    data = [
+        {
+            "participant_id": _+1,
+            "name": fake.name(),
+            "address": fake.street_address(),
+            "age": fake.random_int(min=1, max=99),
+        }
+        for _ in range(30)
+    ]
+
+    return data
