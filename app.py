b716d7e13a61e3e4165917ce27b2fbcdb642ad8e
aydawka <aydan.gasimova2@gmail.com>
2023-05-23 00:08:13 -0700

    chore: added imports for GET POST endpoints

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from apis import api
-
 app = Flask(__name__)
-CORS(app)
-
 api.init_app(app)
+from apis import getStudies, updateStudies, addParticipants
+
+CORS(app)
+app.secret_key = "fairhub_key"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
