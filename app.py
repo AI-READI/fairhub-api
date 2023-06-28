1e7e4fe2ef0aa50591a0e2909729904e6a38231d
aydawka <aydan.gasimova2@gmail.com>
2023-06-28 16:18:41 -0700

    feat: added database keys

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+
 from flask_cors import CORS
 from apis import api
+from flask import jsonify
+import random
 app = Flask(__name__)
 api.init_app(app)
-from apis import getStudies, updateStudies, addParticipants
-
 CORS(app)
-app.secret_key = "fairhub_key"
+
+from apis import getStudies, updateStudies, participants
+
+# db = SQLAlchemy(app)
+
+# app.config.from_object('fairdata.default_settings')
+# app.config.from_envvar('FAIRDATA_SETTINGS')
+
+# db.init_app(app)
 
 
 if __name__ == "__main__":
