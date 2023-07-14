3ead3f096665a327fa58fff3bd965b35981929cf
aydawka <aydan.gasimova2@gmail.com>
2023-07-14 16:19:52 -0700

    fix: flake errors

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from model import db
+from model import *
 from flask_cors import CORS
 from apis.study import study
 from apis.dataset import dataset
 from apis.participant import participant
 from pyfairdatatools import __version__
+import click
 
 app = Flask(__name__)
 app.config.from_prefixed_env("FAIRDATA")
@@ -19,10 +20,14 @@ app.register_blueprint(participant)
 CORS(app)
 
 print(__version__)
+@app.cli.command("echo")
+@click.argument("message")
+def echo(message):
+    print(message)
 
 
 @app.cli.command("create-schema")
-def db_create():
+def echo():
     db.create_all()
 
 
--- model/__init__.py
+++ model/__init__.py
@@ -1,11 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
 from .study import Study
 from .dataset import Dataset
 from .datasetVersion import DatasetVersion
 from .datasetVersions import DatasetVersions
 from .study_contributor import StudyContributor
-from .version_Contributor import VersionContributor
+from .version_contributor import VersionContributor
 from .owner import Owner
 from .participant import Participant
 
-db = SQLAlchemy()
+__all__ = ["Study", "Dataset",'DatasetVersions','DatasetVersion','StudyContributor', 'VersionContributor','Owner',
+           'Participant', "db" ]
