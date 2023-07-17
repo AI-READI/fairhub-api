075a352c866303f08241824ba412720bf9b5234c
Sanjay Soundarajan <sanjay.16@live.com>
2023-07-17 15:48:15 -0700

    🔐 fix: update read statement

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@
 import os
 from dotenv import dotenv_values
 
-FLASK_APP_DATABASE_URL = os.environ.get("FLASK_APP_DATABASE_URL")
+FLASK_APP_DATABASE_URL = os.environ["FLASK_APP_DATABASE_URL"]
 
 # take environment variables from .env
 envcfg = dotenv_values(".env")
