212d2175adc660f6caff7604ee8313887634c344
aydawka <aydan.gasimova2@gmail.com>
2023-07-24 09:33:54 -0700

    wip: replace study and version contributor with m-m relation

--- model/datasetVersion.py
+++ model/datasetVersion.py
@@ -1,13 +1,29 @@
 from .db import db
-from .version_contributor import VersionContributor
+# from .version_contributor import VersionContributor
+import uuid
 
+
+version_contributors = db.Table(
+    "version_contributors",
+    db.Model.metadata,
+    db.Column("datasetVersion_id", db.ForeignKey("datasetVersion.id"), primary_key=True),
+    db.Column("user_id", db.ForeignKey("user.id"), primary_key=True),
+)
+
+
+version_participants = db.Table(
+    "version_participants",
+    db.Model.metadata,
+    db.Column("datasetVersion_id", db.ForeignKey("datasetVersion.id"), primary_key=True),
+    db.Column("participant_id", db.ForeignKey("participant.id"), primary_key=True),
+)
 
 class DatasetVersion(db.Model):
     def __init__(self, dataset):
         self.dataset = dataset
-
+        self.id = str(uuid.uuid4())
     __tablename__ = "datasetVersion"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.CHAR(36), primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     keywords = db.Column(db.String, nullable=False)
@@ -17,12 +33,11 @@ class DatasetVersion(db.Model):
     published = db.Column(db.Boolean, nullable=False)
     DOI = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
-    versionContributors = db.relationship(
-        "VersionContributor", back_populates="datasetVersion"
-    )
+    contributors = db.relationship('User', secondary=version_contributors)
 
-    dataset_id = db.Column(db.Integer, db.ForeignKey("dataset.id"))
+    dataset_id = db.Column(db.CHAR(36), db.ForeignKey("dataset.id"))
     dataset = db.relationship("Dataset", back_populates="datasetVersions")
+    participants = db.relationship('Participant', secondary=version_participants)
 
     def to_dict(self):
         return {
@@ -34,28 +49,31 @@ class DatasetVersion(db.Model):
             "selectedParticipants": self.selectedParticipants,
             "modified": self.modified,
             "published": self.published,
-            "versionContributors": [
-                versionContributor.to_dict()
-                for versionContributor in self.versionContributors
+             "contributors": [
+                 user.to_dict()
+                 for user in self.contributors
             ],
             "DOI": self.DOI,
             "name": self.name,
         }
 
     @staticmethod
-    def from_data(dataset, data):
+    def from_data(data, dataset):
         datasetVersion_obj = DatasetVersion(dataset)
-        # datasetVersion_obj.id = data['id']
-        datasetVersion_obj.title = data["title"]
-        datasetVersion_obj.description = data["description"]
-        datasetVersion_obj.keywords = data["keywords"]
-        datasetVersion_obj.primaryLanguage = data["primaryLanguage"]
-        datasetVersion_obj.selectedParticipants = data["selectedParticipants"]
-        datasetVersion_obj.modified = data["modified"]
-        datasetVersion_obj.published = data["published"]
-        datasetVersion_obj.versionContributors = [
-            VersionContributor.from_data(c) for c in data["versionContributors"]
-        ]
-        datasetVersion_obj.DOI = data["DOI"]
-        datasetVersion_obj.name = data["name"]
+        datasetVersion_obj.update(data)
+        # datasetVersion_obj.versionContributors = [
+        #     VersionContributor.from_data(c) for c in data["versionContributors"]
+        # ]
         return datasetVersion_obj
+
+    def update(self, data):
+        self.title = data["title"]
+        self.description = data["description"]
+        self.keywords = data["keywords"]
+        self.primaryLanguage = data["primaryLanguage"]
+        self.selectedParticipants = data["selectedParticipants"]
+        self.modified = data["modified"]
+        self.published = data["published"]
+
+        self.DOI = data["DOI"]
+        self.name = data["name"]
