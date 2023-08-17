6848d7acb83cb5f3bb35161a7b1a25614f8efefe
aydawka <aydan.gasimova2@gmail.com>
2023-08-17 15:42:21 -0700

    fix:contributor marshall model fixed

--- apis/contributor.py
+++ apis/contributor.py
@@ -1,6 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 
-from model import User
+from model import StudyContributor
 
 api = Namespace("contributor", description="contributors", path="/")
 
@@ -8,15 +8,10 @@ api = Namespace("contributor", description="contributors", path="/")
 contributors_model = api.model(
     "DatasetVersion",
     {
-        "id": fields.String(required=True),
-        "affiliations": fields.String(required=True),
-        "email": fields.String(required=True),
-        "first_name": fields.String(required=True),
-        "last_name": fields.String(required=True),
-        "orcid": fields.String(required=True),
-        "roles": fields.List(fields.String, required=True),
+        "user_id": fields.String(required=True),
         "permission": fields.String(required=True),
-        "status": fields.String(required=True),
+        "study_id": fields.String(required=True),
+
     },
 )
 
@@ -27,8 +22,7 @@ class AddParticipant(Resource):
     @api.response(200, "Success")
     @api.response(400, "Validation Error")
     @api.param("id", "The contributor identifier")
-    # @api.marshal_with(contributors_model)
-    # @api.marshal_with(contributor)
+    @api.marshal_with(contributors_model)
     def get(self, study_id: int):
-        contributors = User.query.all()
+        contributors = StudyContributor.query.all()
         return [c.to_dict() for c in contributors]
