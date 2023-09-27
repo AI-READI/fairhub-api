fcc7e65e57802154885599c256a88beca72c35b6
slugb0t <wheresdorian@gmail.com>
2023-09-27 14:43:33 -0700

    feat: setup tests and fixtures for study api, create dataset api tests

---
+++ tests/functional/test_dataset_api.py
@@ -0,0 +1,16 @@
+"""Tests for API endpoints related to datasets"""
+import json
+
+def test_get_dataset(test_client):
+  """
+  GIVEN a Flask application configured for testing and a study ID
+  WHEN the '/dataset/{study_id}' endpoint is requested (GET)
+  THEN check that the response is valid and retrieves the dataset content
+  """
+
+def test_post_dataset(test_client):
+  """
+  GIVEN a Flask application configured for testing and a study ID
+  WHEN the '/dataset/{study_id}' endpoint is requested (POST)
+  THEN check that the response is valid and creates a dataset
+  """
--- tests/functional/test_study_api.py
+++ tests/functional/test_study_api.py
@@ -1,10 +1,14 @@
 """Tests for API endpoints related to studies"""
 import json
-import os
-from app import create_app
+import pytest
+
+@pytest.fixture()
+def study_id():
+    """A study ID for testing."""
+    return ""
 
 
-def test_post_studies(test_client):
+def test_post_studies(test_client, study_id):
     """
     Given a Flask application configured for testing
     WHEN the '/study' endpoint is requested (POST)
@@ -19,33 +23,46 @@ def test_post_studies(test_client):
             "image": "https://api.dicebear.com/6.x/adventurer/svg",
         },
     )
-    
+    response_data = json.loads(response.data)
+
     assert response.status_code == 200
+    assert response_data["title"] == "Study Title"
+    assert response_data["image"] == "https://api.dicebear.com/6.x/adventurer/svg"
+    study_id = response_data["id"]
 
 
-def test_get_studies(flask_app):
+def test_get_studies(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/study' endpoint is requested (GET)
     THEN check that the response is valid
     """
 
-    # Set the environment to testing
-    os.environ["FLASK_ENV"] = "testing"
+    response = test_client.get("/study")
+    
+    # Convert the response data from JSON to a Python dictionary
+    response_data = json.loads(response.data)
+    
+    print(response_data)
+    # Check the response is correct
+    assert response.status_code == 200
 
-    # Create a test client using the Flask application configured for testing
-    with flask_app.test_client() as test_client:
-        response = test_client.get("/study")
-
-        # Convert the response data from JSON to a Python dictionary
-        response_data = json.loads(response.data)
-        print(response_data)
-
-        # Check the response is correct
-        assert response.status_code == 200
+def test_update_study(test_client):
+    """
+    GIVEN a study ID
+    WHEN the '/study' endpoint is requested (PUT)
+    THEN check that the study is updated
+    """
 
 
-def test_delete_studies_created(flask_app):
+def test_get_study_by_id(test_client):
+    """
+    GIVEN a study ID
+    WHEN the '/study/{study_id}' endpoint is requested (GET)
+    THEN check that the response is valid
+    """
+
+def test_delete_studies_created(test_client):
     """
     Given a Flask application configured for testing
     WHEN the '/study' endpoint is requested (DELETE)
