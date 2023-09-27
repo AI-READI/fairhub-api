8d8574ccb5e820979b251cd7d3c46c7e0fb8e15a
aydawka <aydan.gasimova2@gmail.com>
2023-09-27 10:27:40 -0700

    fix: user details relation is slightly updated due to 1-to-1

--- model/user_details.py
+++ model/user_details.py
@@ -5,6 +5,12 @@ from .db import db
 class UserDetails(db.Model):
     def __init__(self):
         self.id = str(uuid.uuid4())
+        self.first_name = ''
+        self.last_name = ''
+        self.institution = ''
+        self.location = ''
+        self.timezone = ''
+        self.orcid = ''
 
     __tablename__ = "user_details"
     id = db.Column(db.CHAR(36), primary_key=True)
