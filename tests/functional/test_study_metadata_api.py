272995ef06994938783d3e0cc555569dfd19911b
slugb0t <wheresdorian@gmail.com>
2023-09-29 16:47:16 -0700

    feat: tests added for collab, cond, desc, design, eligi, id, inter, ipd, link

--- tests/functional/test_study_metadata_api.py
+++ tests/functional/test_study_metadata_api.py
@@ -49,7 +49,7 @@ def test_delete_arm_metadata(test_client):
     study_id = pytest.global_study_id["id"]
     arm_id = pytest.global_arm_id
     response = test_client.delete(f"/study/{study_id}/metadata/arm/{arm_id}")
-    assert response.status_code == 204
+    assert response.status_code == 200
 
 
 # ------------------- IPD METADATA ------------------- #
@@ -61,7 +61,7 @@ def test_post_available_ipd_metadata(test_client):
     """
     # Endpoint currently not working
     # study_id = pytest.global_study_id["id"]
-    # response = test_client(f"/study/{study_id}/metadata/available_ipd", json={
+    # response = test_client(f"/study/{study_id}/metadata/available-ipd", json={
     #     "identifier": "identifier1",
     #     "type": "type1",
     #     "url": "google.com",
@@ -76,10 +76,43 @@ def test_get_available_ipd_metadata(test_client):
     THEN check that the response is vaild and retrieves the available IPD(s)
     """
     study_id = pytest.global_study_id["id"]
-    response = test_client.get(f"/study/{study_id}/metadata/available_ipd")
+    response = test_client.get(f"/study/{study_id}/metadata/available-ipd")
     assert response.status_code == 200
 
+
 # ------------------- CENTRAL CONTACT METADATA ------------------- #
+def test_post_cc_metadata(test_client):
+    """
+    GIVEN a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/central-contact' endpoint is requested (POST)
+    THEN check that the response is valid and creates the central contact metadata
+    """
+    # BUG: IF CENTRAL_CONTACT IS SET TO FALSE THE ENDPOINT STILL RETURNS AS TRUE
+    study_id = pytest.global_study_id["id"]
+    response = test_client.post(f"/study/{study_id}/metadata/central-contact", json={
+        "name": "central-contact",
+        "affiliation": "affiliation",
+        "role": "role",
+        "phone": "phone",
+        "phone_ext": "phone_ext",
+        "email_address": "email_address",
+        "central_contact": True
+    })
+
+    assert response.status_code == 200
+    response_data = json.loads(response.data)
+    print("$$$$$$$$$$$$$")
+    print(response_data)
+    print("$$$$$$$$$$$$$")
+    assert response_data["central_contact"]["name"] == "central-contact"
+    assert response_data["central_contact"]["affiliation"] == "affiliation"
+    assert response_data["central_contact"]["role"] == "role"
+    assert response_data["central_contact"]["phone"] == "phone"
+    assert response_data["central_contact"]["phone_ext"] == "phone_ext"
+    assert response_data["central_contact"]["email_address"] == "email_address"
+    assert response_data["central_contact"]["central_contact"] == True
+    pytest.global_cc_id = response_data["central_contact"]["id"]
+
 def test_get_cc_metadata(test_client):
     """
     GIVEN a Flask application configured for testing and a study ID
@@ -88,4 +121,217 @@ def test_get_cc_metadata(test_client):
     """
     study_id = pytest.global_study_id["id"]
     response = test_client.get(f"/study/{study_id}/metadata/central-contact")
+    assert response.status_code == 200
+    response_data = json.loads(response.data)
+    print("$$$$$$$$$$$$$")
+    print(response_data)
+    print("$$$$$$$$$$$$$")
+    assert response_data["central_contact"]["name"] == "central-contact"
+    assert response_data["central_contact"]["affiliation"] == "affiliation"
+    assert response_data["central_contact"]["role"] == "role"
+    assert response_data["central_contact"]["phone"] == "phone"
+    assert response_data["central_contact"]["phone_ext"] == "phone_ext"
+    assert response_data["central_contact"]["email_address"] == "email_address"
+    assert response_data["central_contact"]["central_contact"] == True
+
+def test_delete_cc_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID and central contact ID
+    WHEN the '/study/{study_id}/metadata/central-contact/{central_contact_id}' endpoint is requested (DELETE)
+    THEN check that the response is valid and deletes the central contact metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    central_contact_id = pytest.global_cc_id
+    response = test_client.delete(f"/study/{study_id}/metadata/central-contact/{central_contact_id}")
+    assert response.status_code == 200
+
+
+
+#  ------------------- COLLABORATORS METADATA ------------------- #
+def test_get_collaborators_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/collaborators' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the collaborators metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/collaborators")
+    assert response.status_code == 200
+
+# ------------------- CONDITIONS METADATA ------------------- #
+def test_get_conditions_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/conditions' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the conditions metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/conditions")
+    assert response.status_code == 200
+
+
+# ------------------- DESCRIPTION METADATA ------------------- #
+def test_get_description_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/description' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the description metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/description")
+    assert response.status_code == 200
+
+# ------------------- DESIGN METADATA ------------------- #
+def test_get_design_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/design' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the design metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/design")
+    assert response.status_code == 200
+
+# ------------------- ELIGIBILITY METADATA ------------------- #
+def test_get_eligibility_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/eligibility' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the eligibility metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/eligibility")
+    assert response.status_code == 200
+
+# ------------------- IDENTIFICATION METADATA ------------------- #
+def test_get_identification_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/identification' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the identification metadata
+    """
+    # BUG: ENDPOINT NOT WORKING
+    # study_id = pytest.global_study_id["id"]
+    # response = test_client.get(f"/study/{study_id}/metadata/identification")
+    # assert response.status_code == 200
+
+# ------------------- INTERVENTION METADATA ------------------- #
+def test_get_intervention_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/intervention' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the intervention metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/intervention")
+    assert response.status_code == 200
+
+
+# ------------------- IPD SHARING METADATA ------------------- #
+def test_get_ipdsharing_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/ipdsharing' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the ipdsharing metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/ipdsharing")
+    assert response.status_code == 200
+
+
+# ------------------- LINK METADATA ------------------- #
+def test_get_link_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/link' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the link metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/link")
+    assert response.status_code == 200
+
+
+# ------------------- LOCATION METADATA ------------------- #
+def test_get_location_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/location' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the location metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/location")
+    assert response.status_code == 200
+
+
+# ------------------- OTHER METADATA ------------------- #
+def test_get_other_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/other' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the other metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/other")
+    assert response.status_code == 200
+
+
+# ------------------- OVERALL-OFFICIAL METADATA ------------------- #
+def test_get_overall_official_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/overall-official' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the overall-official metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/overall-official")
+    assert response.status_code == 200
+
+
+
+# ------------------- OVERSIGHT METADATA ------------------- #
+def test_get_oversight_official_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/oversight' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the oversight metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/oversight")
+    assert response.status_code == 200
+
+
+# ------------------- REFERENCE METADATA ------------------- #
+def test_get_reference_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/reference' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the reference metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/reference")
+    assert response.status_code == 200
+
+
+# ------------------- SPONSORS METADATA ------------------- #
+def test_get_sponsors_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/sponsors' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the sponsors metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/sponsors")
+    assert response.status_code == 200
+
+
+
+# ------------------- STATUS METADATA ------------------- #
+def test_get_status_metadata(test_client):
+    """
+    Given a Flask application configured for testing and a study ID
+    WHEN the '/study/{study_id}/metadata/status' endpoint is requested (GET)
+    THEN check that the response is valid and retrieves the status metadata
+    """
+    study_id = pytest.global_study_id["id"]
+    response = test_client.get(f"/study/{study_id}/metadata/status")
     assert response.status_code == 200
