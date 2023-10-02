d0b2ff614953eed3c264139e4bfddc3664a198ef
aydawka <aydan.gasimova2@gmail.com>
2023-10-02 10:57:20 -0700

    fix: permissions for contributor DELETE endpoint

--- apis/contributor.py
+++ apis/contributor.py
@@ -35,7 +35,7 @@ class ContributorResource(Resource):
     @api.response(400, "Validation Error")
     @api.expect(contributors_model)
     def put(self, study_id: int, user_id: int):
-        """update contributor permissions"""
+        """update contributor based on the assigned permissions"""
 
         study = Study.query.get(study_id)
         if not is_granted("permission", study):
@@ -67,8 +67,8 @@ class ContributorResource(Resource):
         # TODO: Owners downgrading themselves
         if user != g.user:
             grantee_level = list(grants.keys()).index(grantee.permission) # 2
-            new_level = list(grants.keys()).index(permission) #  0
-            granter_level = list(grants.keys()).index(granter.permission) #2
+            new_level = list(grants.keys()).index(permission) # 0
+            granter_level = list(grants.keys()).index(granter.permission) # 2
             if granter_level <= grantee_level and new_level < grantee_level:
                 return f"User cannot downgrade from {grantee.permission} to {permission}", 403
         grantee.permission = permission
@@ -79,19 +79,33 @@ class ContributorResource(Resource):
     @api.response(200, "Success")
     @api.response(400, "Validation Error")
     def delete(self, study_id: int, user_id: int):
-        contributors = StudyContributor.query.filter_by(
-            study_id=study_id, user_id=user_id).first()
-        if not is_granted("owner", study_id) and contributors.permission == "admin":
-            return "Access denied, you can not delete admin contributors", 403
-        if is_granted("editor", study_id):
-            if contributors.permission == "admin" or contributors.permission == "owner":
-                return "Access denied, editor can not delete admins or owners of the system", 403
-        if is_granted("admin", study_id):
-            if contributors.permission == "admin" or contributors.permission == "owner":
-                return "Access denied, you can not delete other admin's or owner's permissions", 403
-            elif user_id == g.user.id:
-                return "Access denied, you can not assign an owner", 403
-        db.session.delete(contributors)
+        data = request.json
+        study = Study.query.get(study_id)
+        if not is_granted("delete_contributors", study):
+            return "Access denied, you are not authorized to change this permission", 403
+        user = User.query.get(user_id)
+        grantee = StudyContributor.query.filter(
+            StudyContributor.user == user, StudyContributor.study == study
+        ).first()
+
+        granter = StudyContributor.query.filter(
+            StudyContributor.user == g.user, StudyContributor.study == study
+        ).first()
+        # Order should go from the least privileged to the most privileged
+        grants = OrderedDict()
+        grants["viewer"] = []
+        grants["editor"] = ["viewer"]
+        grants["admin"] = ["viewer", "editor"]
+        grants["owner"] = ["editor", "viewer", "admin"]
+
+        # Granter can not downgrade anyone of equal or greater permissions other than themselves
+        # TODO: Owners downgrading themselves
+        if user != g.user:
+            grantee_level = list(grants.keys()).index(grantee.permission)  # 2
+            granter_level = list(grants.keys()).index(granter.permission)  # 2
+            if granter_level <= grantee_level:
+                return f"You are not authorized to delete {grantee.permission}  permission", 403
+        db.session.delete(grantee)
         db.session.commit()
         return 204
 
