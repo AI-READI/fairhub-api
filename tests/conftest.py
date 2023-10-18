42b1c0dbc63c795b1dc80be57d0e032b59deca12
slugb0t <wheresdorian@gmail.com>
2023-10-17 17:51:41 -0700

    fix: update pytest fixtures for validation schemas

--- tests/conftest.py
+++ tests/conftest.py
@@ -63,7 +63,7 @@ def _create_user(_test_client):
     with unittest.mock.patch("pytest_config.TestConfig", TestConfig):
         response = _test_client.post(
             "/auth/signup",
-            json={"email_address": "sample@gmail.com", "password": "test"},
+            json={"email_address": "sample@gmail.com", "password": "Testingyeshello11!"},
         )
 
         assert response.status_code == 201
@@ -76,7 +76,7 @@ def _login_user(_test_client):
     with unittest.mock.patch("pytest_config.TestConfig", TestConfig):
         response = _test_client.post(
             "/auth/login",
-            json={"email_address": "sample@gmail.com", "password": "test"},
+            json={"email_address": "sample@gmail.com", "password": "Testingyeshello11!"},
         )
 
         assert response.status_code == 200
