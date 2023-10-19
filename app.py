47dade9cdc24b75bcdc9dd6dc7515c4a1dd5df24
Sanjay Soundarajan <sanjay.16@live.com>
2023-10-19 10:56:12 -0700

    🚨 chore: fix flake errors

--- app.py
+++ app.py
@@ -61,14 +61,15 @@ def create_app(config_module=None):
     api.init_app(app)
     bcrypt.init_app(app)
 
-    # Only allow CORS origin for localhost:3000 and any subdomain of azurestaticapps.net/
+    # Only allow CORS origin for localhost:3000
+    # and any subdomain of azurestaticapps.net/
     CORS(
         app,
         resources={
             "/*": {
                 "origins": [
                     "http://localhost:3000",
-                    "https://brave-ground-07b6bfb10-datasetmetadata.centralus.2.azurestaticapps.net",
+                    "https://brave-ground-*.centralus.2.azurestaticapps.net",
                 ],
             }
         },
