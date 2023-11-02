8d24d1968ccd49f9d52dc7b13754735083d5ff63
Greenstick <benacordier@gmail.com>
2023-11-01 17:14:50 -0700

    Merge branch 'staging' of https://github.com/AI-READI/api.fairhub.io into staging - cleanup

--- config.py
+++ config.py
@@ -5,15 +5,10 @@ from dotenv import dotenv_values
 
 # Check if `.env` file exists
 env_path = Path(".") / ".env"
-
 LOCAL_ENV_FILE = env_path.exists()
 
 # Load environment variables from .env
 config = dotenv_values(".env")
-
-<<<<<<< HEAD
-from dotenv import load_dotenv
-load_dotenv(".env")
 
 FAIRHUB_DATABASE_URL = environ.get("FAIRHUB_DATABASE_URL")
 FAIRHUB_SECRET = environ.get("FAIRHUB_SECRET")
@@ -23,7 +18,6 @@ CACHE_HOST = environ.get("CACHE_HOST")
 CACHE_PORT = environ.get("CACHE_PORT")
 CACHE_DB = environ.get("CACHE_DB")
 CACHE_URL = environ.get("CACHE_URL")
-=======
 
 def get_env(key):
     """Return environment variable from .env or native environment."""
@@ -35,4 +29,3 @@ FAIRHUB_SECRET = get_env("FAIRHUB_SECRET")
 
 FAIRHUB_AZURE_READ_SAS_TOKEN = get_env("FAIRHUB_AZURE_READ_SAS_TOKEN")
 FAIRHUB_AZURE_STORAGE_ACCOUNT_NAME = get_env("FAIRHUB_AZURE_STORAGE_ACCOUNT_NAME")
->>>>>>> fb3fb5cace0ebc1c41c9914afad862ec4a889831
