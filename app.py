eceaa8f9ec719e47b5969973dda5bf556d850cc4
Greenstick <benacordier@gmail.com>
2023-11-02 13:22:32 -0700

    🐛 fix: return default port to 5000

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument(
-        "-p", "--port", default=3001, type=int, help="port to listen on"
+        "-p", "--port", default=5000, type=int, help="port to listen on"
     )
     args = parser.parse_args()
     port = args.port
