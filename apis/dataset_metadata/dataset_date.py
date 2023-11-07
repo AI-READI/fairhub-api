e074add4ee9948ce6a0c0fe621ce916793e37c16
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2023-11-07 13:11:33 -0800

    ✨ feat: add support for jsonschema validation for dataset metadata (#20)
    * ✨ feat: add support for jsonschema validation for dataset metadata
    
    * 🐛 fix: add better validation for related items

--- apis/dataset_metadata/dataset_date.py
+++ apis/dataset_metadata/dataset_date.py
@@ -1,7 +1,9 @@
+"""APIs for dataset date metadata"""
 from typing import Any, Union
 
 from flask import request
 from flask_restx import Resource, fields
+from jsonschema import ValidationError, validate
 
 import model
 from apis.authentication import is_granted
@@ -20,11 +22,14 @@ dataset_date = api.model(
 
 @api.route("/study/<study_id>/dataset/<dataset_id>/metadata/date")
 class DatasetDateResource(Resource):
+    """Dataset Date Resource"""
+
     @api.doc("date")
     @api.response(200, "Success")
     @api.response(400, "Validation Error")
     # @api.marshal_with(dataset_date)
     def get(self, study_id: int, dataset_id: int):  # pylint: disable= unused-argument
+        """Get dataset date"""
         dataset_ = model.Dataset.query.get(dataset_id)
         dataset_date_ = dataset_.dataset_date
         return [d.to_dict() for d in dataset_date_]
@@ -33,9 +38,40 @@ class DatasetDateResource(Resource):
     @api.response(200, "Success")
     @api.response(400, "Validation Error")
     def post(self, study_id: int, dataset_id: int):
+        """Update dataset date"""
         study_obj = model.Study.query.get(study_id)
+
         if not is_granted("dataset_metadata", study_obj):
             return "Access denied, you can not make any change in dataset metadata", 403
+
+        schema = {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "additionalProperties": False,
+                "properties": {
+                    "id": {"type": "string"},
+                    "date": {
+                        "type": "integer",
+                    },
+                    "type": {
+                        "type": "string",
+                        "minLength": 1,
+                    },
+                    "information": {
+                        "type": "string",
+                    },
+                },
+                "required": ["date", "type", "information"],
+            },
+            "uniqueItems": True,
+        }
+
+        try:
+            validate(instance=request.json, schema=schema)
+        except ValidationError as err:
+            return err.message, 400
+
         data: Union[Any, dict] = request.json
         data_obj = model.Dataset.query.get(dataset_id)
         list_of_elements = []
@@ -56,12 +92,15 @@ class DatasetDateResource(Resource):
 
 @api.route("/study/<study_id>/dataset/<dataset_id>/metadata/date/<date_id>")
 class DatasetDateDeleteResource(Resource):
+    """Dataset Date Delete Resource"""
+
     @api.doc("delete date")
     @api.response(200, "Success")
     @api.response(400, "Validation Error")
     def delete(
         self, study_id: int, dataset_id: int, date_id: int
     ):  # pylint: disable= unused-argument
+        """Delete dataset date"""
         study_obj = model.Study.query.get(study_id)
         if not is_granted("dataset_metadata", study_obj):
             return "Access denied, you can not make any change in dataset metadata", 403
