be145956829b06ac696c29b40c5af9ba8142dc14
slugb0t <wheresdorian@gmail.com>
2023-11-15 15:09:48 -0800

    admin, editor, viewer permission check created for GET identification metadata

--- tests/functional/test_study_metadata_api.py
+++ tests/functional/test_study_metadata_api.py
@@ -1054,8 +1054,14 @@ def test_get_identification_metadata(clients):
     study_id = pytest.global_study_id["id"]  # type: ignore
 
     response = _logged_in_client.get(f"/study/{study_id}/metadata/identification")
+    admin_response = _admin_client.get(f"/study/{study_id}/metadata/identification")
+    editor_response = _editor_client.get(f"/study/{study_id}/metadata/identification")
+    viewer_response = _viewer_client.get(f"/study/{study_id}/metadata/identification")
 
     assert response.status_code == 200
+    assert admin_response.status_code == 200
+    assert editor_response.status_code == 200
+    assert viewer_response.status_code == 200
 
 
 def test_post_identification_metadata(clients):
