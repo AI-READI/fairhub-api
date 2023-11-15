91d6664509c063be0853c9dafce6e27ea604591f
slugb0t <wheresdorian@gmail.com>
2023-11-15 13:43:45 -0800

    admin, editor, viewer permission check created for GET description metadata

--- tests/functional/test_study_metadata_api.py
+++ tests/functional/test_study_metadata_api.py
@@ -610,8 +610,14 @@ def test_get_description_metadata(clients):
     study_id = pytest.global_study_id["id"]  # type: ignore
 
     response = _logged_in_client.get(f"/study/{study_id}/metadata/description")
+    admin_response = _admin_client.get(f"/study/{study_id}/metadata/description")
+    editor_response = _editor_client.get(f"/study/{study_id}/metadata/description")
+    viewer_response = _viewer_client.get(f"/study/{study_id}/metadata/description")
 
     assert response.status_code == 200
+    assert admin_response.status_code == 200
+    assert editor_response.status_code == 200
+    assert viewer_response.status_code == 200
 
 
 def test_put_description_metadata(clients):
