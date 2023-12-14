f89d26b5038bc597c021152cdc9a2cce3c5e95f5
aydawka <aydan.gasimova2@gmail.com>
2023-12-14 12:05:43 -0800

    wip: notification endpoint

--- model/notification.py
+++ model/notification.py
@@ -6,17 +6,18 @@ from .db import db
 
 
 class Notification(db.Model):  # type: ignore
-    def __init__(self):
+    def __init__(self, user):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now(timezone.utc).timestamp()
+        self.user = user
 
     __tablename__ = "notification"
     id = db.Column(db.CHAR(36), primary_key=True)
-    title = db.Column(db.String, nullable=True)
-    message = db.Column(db.String, nullable=True)
-    type = db.Column(db.String, nullable=True)
-    target = db.Column(db.String, nullable=True)
-    read = db.Column(db.BOOLEAN, nullable=True)
+    title = db.Column(db.String, nullable=False)
+    message = db.Column(db.String, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    target = db.Column(db.String, nullable=False)
+    read = db.Column(db.BOOLEAN, nullable=False)
     created_at = db.Column(db.BigInteger, nullable=False)
 
     user_id = db.Column(db.CHAR(36), db.ForeignKey("user.id"), nullable=False)
@@ -34,10 +35,10 @@ class Notification(db.Model):  # type: ignore
         }
 
     @staticmethod
-    def from_data(data: dict):
-        user = Notification()
-        user.update(data)
-        return user
+    def from_data(user, data: dict):
+        notification = Notification(user)
+        notification.update(data)
+        return notification
 
     def update(self, data: dict):
         self.title = data["title"]
