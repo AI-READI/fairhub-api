2a9426091196fe218aa3b4567e72a8bae95b0889
Lint Action <lint-action@samuelmeuli.com>
2023-12-15 05:46:01 +0000

    style: 🎨 fix code style issues with Black

--- alembic/versions/f150341d2741_modify_email_verification.py
+++ alembic/versions/f150341d2741_modify_email_verification.py
@@ -12,8 +12,8 @@ import datetime
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'f150341d2741'
-down_revision: Union[str, None] = 'b20e07d8924f'
+revision: str = "f150341d2741"
+down_revision: Union[str, None] = "b20e07d8924f"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -21,14 +21,15 @@ created_at = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
 
 
 def upgrade() -> None:
+    op.alter_column("email_verification", "token", type_=sa.String)
+    op.alter_column("email_verification", "user_id", type_=sa.CHAR(36))
 
-        op.alter_column('email_verification', 'token', type_=sa.String)
-        op.alter_column('email_verification', 'user_id', type_=sa.CHAR(36))
+    op.drop_column("email_verification", "created_at")
 
-        op.drop_column('email_verification', 'created_at')
+    op.add_column(
+        "email_verification", sa.Column("created_at", sa.BIGINT(), nullable=True)
+    )
 
-        op.add_column('email_verification', sa.Column('created_at', sa.BIGINT(), nullable=True))
+    op.execute(f"UPDATE \"email_verification\" SET created_at ='{created_at}'")
 
-        op.execute(f'UPDATE "email_verification" SET created_at =\'{created_at}\'')
-
-        op.alter_column('email_verification', 'created_at', nullable=False)
+    op.alter_column("email_verification", "created_at", nullable=False)
