369180a87b5312a509538594429a09b40b41fc3c
slugb0t <wheresdorian@gmail.com>
2023-12-20 12:38:10 -0800

    :sparkles: feat: tests for logging in user after password changes

--- tests/functional/test_user.py
+++ tests/functional/test_user.py
@@ -8,7 +8,7 @@ def test_post_password_change(clients):
     WHEN the '/auth/password/change' endpoint is requested (PUT)
     THEN check that the response is valid and the password is changed
     """
-    _logged_in_client, _admin_client, _editor_client, _viewer_client = clients
+    _logged_in_client = clients[0]
 
     response = _logged_in_client.post(
         "/auth/password/change",
@@ -16,6 +16,42 @@ def test_post_password_change(clients):
             "confirm_password": "Updatedpassword4testing!",
             "new_password": "Updatedpassword4testing!",
             "old_password": "Testingyeshello11!",
+        },
+    )
+
+    assert response.status_code == 200
+
+
+def test_post_password_login_invalid_old_password(clients):
+    """
+    Given a Flask application configured for testing
+    WHEN the '/auth/login' endpoint is requested (POST)
+    THEN check that the response is an error when old password is provided
+    """
+    _logged_in_client = clients[0]
+    response = _logged_in_client.post(
+        "/auth/login",
+        json={
+            "email_address": "test@fairhub.io",
+            "password": "Testingyeshello11!",
+        },
+    )
+
+    assert response.status_code == 401
+
+
+def test_post_login_new_password(clients):
+    """
+    Given a Flask application configured for testing
+    WHEN the '/auth/login' endpoint is requested (POST)
+    THEN check that the response is valid when new password is provided
+    """
+    _logged_in_client = clients[0]
+    response = _logged_in_client.post(
+        "/auth/login",
+        json={
+            "email_address": "test@fairhub.io",
+            "password": "Updatedpassword4testing!",
         },
     )
 
