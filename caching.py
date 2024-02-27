489583ba132827c65647c00573e9fb0a334fe0e1
Sanjay Soundarajan <sanjay.16@live.com>
2024-02-26 17:37:04 -0800

    🚚 chore: update path

--- caching.py
+++ caching.py
@@ -3,7 +3,7 @@ from config import config
 
 cache = Cache(
     config={
-        key.replace(f"FAIRHUB_", ""): value
+        key.replace("FAIRHUB_", ""): value
         for key, value in config.items()
         if "CACHE" in key
     }
