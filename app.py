3e4bfd40657dc4f9e93f6903a96ed5df375e0109
aydawka <aydan.gasimova2@gmail.com>
2024-06-28 16:39:13 -0700

    fix: remove alembic table

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import Flask, g, request
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from growthbook import GrowthBook
-from sqlalchemy import MetaData, inspect
+from sqlalchemy import MetaData, inspect, text
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.schema import DropTable
 from waitress import serve
@@ -132,10 +132,9 @@ def create_app(config_module=None, loglevel="INFO"):
         if config.FAIRHUB_DATABASE_URL.find("azure") > -1:
             return
         engine = model.db.session.get_bind()
-        with engine.begin():
+        with engine.begin() as conn:
             model.db.drop_all()
-            # conn.execute(text("DROP TABLE IF EXISTS alembic_version"))
-
+            conn.execute(text("DROP TABLE IF EXISTS alembic_version"))  # type: ignore
     @app.cli.command("cycle-schema")
     def cycle_schema():
         """Destroy then re-create the database schema."""
@@ -260,7 +259,7 @@ def create_app(config_module=None, loglevel="INFO"):
         )
 
         new_token = jwt.encode(
-            {"user": decoded["user"], "exp": expired_in,  "jti": decoded["jti"]},
+            {"user": decoded["user"], "exp": expired_in, "jti": decoded["jti"]},
             config.FAIRHUB_SECRET,
             algorithm="HS256",
         )
