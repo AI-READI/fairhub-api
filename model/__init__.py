f78ff2eee3a533820cf762172ca003328f1852ba
Sanjay Soundarajan <sanjay.16@live.com>
2024-08-01 13:02:11 -0700

    Revert "feat: add sessions (#56)"
    This reverts commit 4f7fc0da4b5dcc2e9298b9c702ef0f6b938b36c6.

--- model/__init__.py
+++ model/__init__.py
@@ -48,7 +48,6 @@ from .token_blacklist import TokenBlacklist
 from .user import User
 from .user_details import UserDetails
 from .version import Version
-from .session import Session
 from .version_readme import VersionReadme
 
 __all__ = [
@@ -103,5 +102,4 @@ __all__ = [
     "UserDetails",
     "Notification",
     "VersionReadme",
-    "Session",
 ]
