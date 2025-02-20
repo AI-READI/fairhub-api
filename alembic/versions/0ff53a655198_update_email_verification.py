cb3388f047aa79dc68a55cd0a65918d4189472a9
aydawka <aydan.gasimova2@gmail.com>
2025-02-19 17:59:42 -0800

    fix: alembic

--- alembic/versions/0ff53a655198_update_email_verification.py
+++ alembic/versions/0ff53a655198_update_email_verification.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '0ff53a655198'
-down_revision: Union[str, None] = '3ffefbd9c03b'
+down_revision: Union[str, None] = '5c1257547eb8'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
